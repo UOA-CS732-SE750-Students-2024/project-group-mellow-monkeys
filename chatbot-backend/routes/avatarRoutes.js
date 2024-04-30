f45caa8360a87f12e061a43f9a20e810a2cd8194
Alex nan <mapledeveloper@outlook.com>
2024-04-30 16:55:10 +1200

    Add generating avatarRoutes

---
+++ chatbot-backend/routes/avatarRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios"
+const router = express.Router();
+
+router.post('/generate-avatar', async (req, res) => {
+  const { description } = req.body;
+  const OPENAI_API_KEY = process.env.API_KEY;
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/images/generations', {
+      prompt: description,
+      n: 1,
+      size: "1024x1024"
+    }, {
+      headers: {
+        'Authorization': `Bearer ${OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const imageURL = response.data.data[0].url;
+    res.json({ imageURL });
+  } catch (error) {
+    console.error('Failed to generate avatar:', error);
+    res.status(500).send('Error generating avatar: ' + error.message);
+  }
+});
+
+export default router;
--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -6,9 +6,12 @@ import { connectWithRetry } from "./data/init-db.js";
 import userRoutes from "./routes/users.js";
 import chatBotRoutes from "./routes/chatbots.js";
 import openAIRoutes from "./routes/openAI.js";
+import avatarRoutes from "./routes/avatarRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 8001;
+
+// app.use('/api', avatarRoutes);
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +19,8 @@ app.use(cors());
 app.use("/", userRoutes);
 app.use("/", chatBotRoutes);
 app.use("/", openAIRoutes);
+app.use("/", avatarRoutes); 
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the API!" });
